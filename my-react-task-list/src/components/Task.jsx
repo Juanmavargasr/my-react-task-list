0a8c8f2a7d25b0696ea8385805aaba872660a678
Juan Manuel Vargas <juanmavargasr@gmail.com>
2023-03-29 15:18:55 -0500

    correcting discard word

--- my-react-task-list/src/components/Task.jsx
+++ my-react-task-list/src/components/Task.jsx
@@ -5,7 +5,7 @@ function Task(props){
     return (
         <li>
             <input type="checkbox" /> <label htmlFor="">{taskname}</label>
-            <button onClick={alert(`The task ${taskname} has been discarded`)} >Disscard task</button>
+            <button onClick={alert(`The task ${taskname} has been discarded`)} >Discard task</button>
         </li>
     );
 }
