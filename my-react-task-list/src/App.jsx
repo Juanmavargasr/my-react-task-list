5296e5f42711cd1f8d2b41163e9f10d3e47b321b
Juan Manuel Vargas <juanmavargasr@gmail.com>
2023-04-18 16:29:46 -0500

    updating css code

--- my-react-task-list/src/App.jsx
+++ my-react-task-list/src/App.jsx
@@ -2,9 +2,11 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap" />
 import { useEffect } from "react";
 import Header from "./components/Header";
 import Taskslist from "./components/Taskslist";
+import './index.css';
 
 
 
