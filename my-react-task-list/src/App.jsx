53cf6afd56d60704f8979433edaa4395f83e7033
Juan Manuel Vargas <juanmavargasr@gmail.com>
2023-05-15 12:15:46 -0500

    Now, the context work and we could separate the pending task of the done task.

--- my-react-task-list/src/App.jsx
+++ my-react-task-list/src/App.jsx
@@ -1,25 +1,12 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-import { useEffect } from "react";
 import Header from "./components/Header";
-import Taskslist from "./components/Taskslist";
-// import context from "./components/Context";
-// import Donetask from "./components/Donetask";
-
 import "./index.css";
 import Context from "./components/Context";
-import Task from "./components/Task";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h3>Pending Task</h3>
-      <Taskslist />
-      <h3>Done Task</h3>
-      {/* <Donetask/> */}
+      <Context />
     </div>
   );
 }
--- my-react-task-list/src/components/Task.jsx
+++ my-react-task-list/src/components/Task.jsx
@@ -1,26 +1,31 @@
-function Task(props){
+function Task(props) {
+  const { id, taskname, description, completed, onModificarElemento } = props;
 
-    const {id, taskname, description, completed, onModificarElemento} = props;
+  function handleClickDiscarded() {
+    alert(`The task ${taskname} has been discarded.`);
+  }
 
-
-    function handleClickDiscarded() {
-        alert(`The task ${taskname} has been discarded.`)
-    }
-
-    return (
-        <li>
-            <input type="checkbox"
-                name={taskname}  
-                onClick={() => {onModificarElemento(id)}}
-                onChange={() => {}}
-                checked = {completed}
-            />
-            <label><h3>{taskname}</h3></label>
-            <label> <p>{description}</p></label>
-            <button onClick={handleClickDiscarded}>Discard task</button>
-        </li>
-    );
+  return (
+    <li>
+      <input
+        type="checkbox"
+        name={taskname}
+        onClick={() => {
+          onModificarElemento(id);
+        }}
+        onChange={() => {}}
+        checked={completed}
+      />
+      <label>
+        <h3>{taskname}</h3>
+      </label>
+      <label>
+        {" "}
+        <p>{description}</p>
+      </label>
+      <button onClick={handleClickDiscarded}>Discard task</button>
+    </li>
+  );
 }
 
 export default Task;
-
