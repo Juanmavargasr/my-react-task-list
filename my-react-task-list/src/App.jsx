c623c682be57261e3c0d0d0ba23ce018d92bcb63
Juan Manuel Vargas <juanmavargasr@gmail.com>
2023-05-16 19:16:38 -0500

    new css

--- my-react-task-list/src/App.jsx
+++ my-react-task-list/src/App.jsx
@@ -1,12 +1,108 @@
 import Header from "./components/Header";
 import "./index.css";
 import Context from "./components/Context";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [description, SetDescription] = useState("");
+  const [formValidation, setFormValidation] = useState({
+    name: undefined,
+    description: undefined,
+  });
+
+  const handleNameChange = (event) => {
+    const value = event.target.value;
+    setFormValidation({
+      ...formValidation,
+      name:
+        value.length > 2
+          ? ""
+          : "The taskname is to short, should have at least 3 character",
+    });
+    setName();
+  };
+
+  const handleDescriptionChange = (event) => {
+    SetDescription();
+  };
+
+  // const nameExist = (formValidation) => {
+  //   if (formValidation.name !== "") {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // };
+
+  const isFormValid = formValidation.name === "";
+
+  //   return (
+  //     <div>
+  //       <Header />
+  //       <form>
+  //         <div>
+  //           <label>Taskname</label>
+  //           <input
+  //             value={name}
+  //             type="text"
+  //             placeholder="Taskname"
+  //             onChange={handleNameChange}
+  //           />
+  //           <label> Task Description</label>
+  //           <input
+  //             value={description}
+  //             type="text"
+  //             placeholder="Task Description"
+  //             onChange={handleDescriptionChange}
+  //           />
+  //           <button disabled={!isFormValid}>Create</button>
+  //           <span className="error" role="alert">
+  //             {formValidation.name}
+  //           </span>
+  //         </div>
+  //       </form>
+  //       <div className="App">
+  //         <Context />
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
   return (
-    <div className="App">
-      <Header />
-      <Context />
+    <div className="container">
+      <div className="header">
+        <h2 className="title">Task List</h2>
+        <form className="form">
+          <div>
+            <label className="form-label">Taskname</label>
+            <input
+              className="form-input"
+              value={name}
+              type="text"
+              placeholder="Taskname"
+              onChange={handleNameChange}
+            />
+            <label className="form-label"> Description</label>
+            <input
+              className="form-input"
+              value={description}
+              type="text"
+              placeholder="Task Description"
+              onChange={handleDescriptionChange}
+            />
+            <button className="create-button" disabled={!isFormValid}>
+              Create
+            </button>
+            <span className="error" role="alert">
+              {formValidation.name}
+            </span>
+          </div>
+        </form>
+      </div>
+      <div className="task-list">
+        <Context />
+      </div>
     </div>
   );
 }
